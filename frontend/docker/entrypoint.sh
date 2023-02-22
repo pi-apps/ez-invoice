40d57d3b5780ebd2c31fad8b55f9810e721a3c45
Dev Super Pi <khanghy.india@gmail.com>
2023-02-22 14:02:37 +0700

    update api google

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -15,6 +15,7 @@ fi
 # https://unix.stackexchange.com/questions/379572/escaping-both-forward-slash-and-back-slash-with-sed
 sed -i "s;%REACT_APP_BACKEND_URL%;${BACKEND_URL};" /var/www/webapp/index.html
 sed -i "s;%REACT_APP_SANDBOX_SDK%;${SANDBOX_SDK};" /var/www/webapp/index.html
+sed -i "s;%REACT_APP_APIKEY_GOOGLE%;${APIKEY_GOOGLE};" /var/www/webapp/index.html
 
 # Call the command that the base image was initally supposed to run
 # See: XXX
