c008b9feb4915acdafc43aa2531e12b5b9ab951c
Dev Pibridge <khanghy.india@gmail.com>
2023-02-22 13:43:17 +0700

    fix import Translate

--- frontend/src/views/CreateInvoices/components/CreateDetail/index.tsx
+++ frontend/src/views/CreateInvoices/components/CreateDetail/index.tsx
@@ -14,6 +14,7 @@ import Footer from '../Footer';
 import { InvoiceIdContext } from 'contexts/InVoiceIdContext';
 import { useContext, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Translate } from "react-auto-translate";
 import { GetTranslateHolder } from 'hooks/TranSlateHolder';
 
 const CreateDetail = () => {
